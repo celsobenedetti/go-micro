e2b31f14c63436e1114f97140fa0cfe3df33e091
Celso Benedetti <celso.benedetti@dell.com>
2023-02-19 15:54:34 -0300

    new(min): broker-service communicating w/ web frontend through REST

---
+++ front-end/cmd/web/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+const webPort = "5000"
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		render(w, "test.page.gohtml")
+	})
+
+
+    port := fmt.Sprintf(":%s", webPort)
+	fmt.Printf("Starting front end service on port %s\n", port)
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func render(w http.ResponseWriter, t string) {
+
+	partials := []string{
+		"./cmd/web/templates/base.layout.gohtml",
+		"./cmd/web/templates/header.partial.gohtml",
+		"./cmd/web/templates/footer.partial.gohtml",
+	}
+
+	var templateSlice []string
+	templateSlice = append(templateSlice, fmt.Sprintf("./cmd/web/templates/%s", t))
+
+	for _, x := range partials {
+		templateSlice = append(templateSlice, x)
+	}
+
+	tmpl, err := template.ParseFiles(templateSlice...)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		
+	}
+
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
