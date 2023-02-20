905f6230a0d4b57642224814d031f5247fccedf4
Celso Benedetti <celso.benedetti@dell.com>
2023-02-20 18:38:48 -0300

    new: auth service pg connection setup

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -5,33 +5,87 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 
 	"github.com/celso-patiri/celso-patiri/go-micro/authentication/data"
 )
 
 const webPort = "80"
 
+var connectionTries int
+
 type Config struct {
-    DB *sql.DB
-    Models data.Models
+	DB     *sql.DB
+	Models data.Models
 }
 
 func main() {
-    log.Println("Starting authentication service")
+	log.Println("Starting authentication service")
 
-    //TODO: connect to db
+	//TODO: connect to db
+	conn := connectToDb()
+	if conn == nil {
+		log.Panic("Can't connect to Postgres")
+	}
 
-    // set up config 
+	// set up config
+	app := &Config{
+		DB:     conn,
+		Models: data.New(conn),
+	}
 
-    app := &Config{}
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
 
-    srv := &http.Server{
-        Addr: fmt.Sprintf(":%s", webPort),
-        Handler: app.routes(),
-    }
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
 
-    err := srv.ListenAndServe()
-    if err != nil {
-        log.Panic(err)
-    }
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func connectToDb() *sql.DB {
+	dsn := os.Getenv("DSN")
+
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Postgres not yet ready, ", err)
+			connectionTries++
+		} else {
+			log.Println("Connected to Postgres")
+			return connection
+		}
+
+		if connectionTries > 10 {
+			log.Println(err)
+			return nil
+		}
+
+		wait := time.Duration(2)
+		log.Printf("Waiting for %d seconds...\n", wait)
+		time.Sleep(wait * time.Second)
+	}
 }
